fb792f016864b9f9084084d3bb747e003179da22
jblomquist2 <159498283+jblomquist2@users.noreply.github.com>
2024-11-23 20:16:03 +0000

    Updated Code

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ AllowedVehiclesList = ["Ford F-150", "Chevrolet Silverado", "Tesla Cybertruck", 
 print("********************************")
 print("AutoCountry Vehicle Finder v0.1")
 print("********************************")
-print("Please Enter the following number below from the following menu:")
+print("Pleas Enter the following number below from the following menu: ")
 print("")
 print("1. PRINT all Authorized Vehicles")
 print("2. Exit")
 
-while True:
-  number = input(int)
-  if number == "1":
-    print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
-    for truck in AllowedVehiclesList:
-      print(truck)
-  elif number =="2":
-      print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
-      break
+number = int(input())
+if number == 1:
+  print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
+  for truck in AllowedVehiclesList:
+    print(truck)
+  
+elif number == 2:
+  print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
